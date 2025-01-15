781958ebccc959ad0101a6ffbe1ad3555369ecaa
shacharshemesh <shacharsms10@gmail.com>
2025-01-15 17:28:06 +0200

    add logic

--- app/src/main/java/com/example/xoproject/MainActivity.kt
+++ app/src/main/java/com/example/xoproject/MainActivity.kt
@@ -2,15 +2,23 @@ package com.example.xoproject
 
 import android.graphics.Color
 import android.os.Bundle
+import android.view.Gravity
 import android.widget.Button
 import android.widget.GridLayout
+import android.widget.LinearLayout
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.example.xoproject.logic.EMPTY_CELL
+import com.example.xoproject.logic.Result
+import com.example.xoproject.logic.XOGame
+import com.example.xoproject.logic.X_SYMBOL
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var game: XOGame
     private lateinit var buttons: Array<Array<Button>>
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,10 +31,13 @@ class MainActivity : AppCompatActivity() {
             insets
         }
 
+        game = XOGame()
         buttons = Array(3) { row ->
             Array(3) { column ->
-                Button(this).apply {
-                    textSize = 26f
+                val b = Button(this)
+                b.apply {
+                    textSize = 60f
+
                     setOnClickListener { handleMove(row, column) }
                 }
             }
@@ -41,10 +52,8 @@ class MainActivity : AppCompatActivity() {
                 val button = buttons[row][column]
 
                 val params = GridLayout.LayoutParams().apply {
-                    width = 0
-                    height = 0
-                    rowSpec = GridLayout.spec(row, 1f)
-                    columnSpec = GridLayout.spec(column, 1f)
+                    rowSpec = GridLayout.spec(row, 1)
+                    columnSpec = GridLayout.spec(column, 1)
                 }
 
                 button.layoutParams = params
@@ -52,10 +61,54 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+        gridLayout.layoutParams = LinearLayout.LayoutParams(
+            LinearLayout.LayoutParams.WRAP_CONTENT,
+            LinearLayout.LayoutParams.WRAP_CONTENT
+        ).apply {
+            gravity = Gravity.CENTER
+        }
+
+        findViewById<Button>(R.id.activity_main_reset_button).setOnClickListener {
+            game.reset()
+            updateBoard()
+        }
+
+        updateBoard()
 
     }
 
     private fun handleMove(row: Int, col: Int) {
-        Toast.makeText(this, "click", Toast.LENGTH_LONG).show()
+        if(game.validateTurn(row, col)) {
+            game.move(row, col)
+            updateBoard()
+            val winner = game.checkForWinner()
+            if (winner != null) {
+                val message = when (winner) {
+                    Result.X_SYMBOL -> "X is the Winner!!"
+                    Result.O_SYMBOL -> "O is the Winner!"
+                    Result.DRAW -> "It's a draw!"
+                }
+                Toast.makeText(this, message, Toast.LENGTH_LONG).show()
+                game.reset()
+            }
+        }
     }
+
+    private fun playerColor(player: String) : Int {
+        return if (player == X_SYMBOL.toString()) Color.RED else Color.BLUE
+    }
+
+    private fun updateBoard() {
+        val board = game.board
+        for (row in board.indices) {
+            for (column in board[row].indices) {
+                val currCell = board[row][column].toString()
+                buttons[row][column].text = currCell
+                buttons[row][column].setTextColor(playerColor(currCell))
+                buttons[row][column].isEnabled = board[row][column] == EMPTY_CELL
+            }
+        }
+    }
+
+
 }
